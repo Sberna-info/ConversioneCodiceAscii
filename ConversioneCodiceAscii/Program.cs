c0b23039ae740a04414b94b21ee534a7bbbaa327
andry <andry@DESKTOP-SENM7HG>
2020-11-10 12:19:15 +0100

    Add project files.

---
+++ ConversioneCodiceAscii/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversioneCodiceAscii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inserisci la lettere che vuoi convertire: ");
+            char carattere = Convert.ToChar(Console.ReadLine());
+            ushort carattereascii = 0;
+            carattereascii = carattere;
+            Console.WriteLine($"la lettera - {carattere}, Carattere in ascii - {carattereascii}");
+            Console.ReadLine();
+
+        }
+    }
+}
